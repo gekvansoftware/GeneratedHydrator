87ec586500ae3dde5e3dcd262c6142668e760cb5
Jens de Nies <j.de.nies@protonmail.com>
2020-07-06 15:30:48 +0200

    Changes.

--- src/GeneratedHydrator/CodeGenerator/Visitor/HydratorMethodsVisitor.php
+++ src/GeneratedHydrator/CodeGenerator/Visitor/HydratorMethodsVisitor.php
@@ -124,7 +124,7 @@ class HydratorMethodsVisitor extends NodeVisitorAbstract
 
         $result = [
             'if (isset(' . $inputArrayName . '[' . $escapedName . '])',
-            '    || $object->' . $propertyName . ' !== null && \\array_key_exists(' . $escapedName . ', ' . $inputArrayName . ')',
+            '    || isset($object->' . $propertyName . ') && \\array_key_exists(' . $escapedName . ', ' . $inputArrayName . ')',
             ') {'
         ];
 
